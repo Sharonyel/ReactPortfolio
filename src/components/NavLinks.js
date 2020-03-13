11dd11b0e68d56eb7cc41614d64dff13f73ad483
Sharon Yelverton <ladysharonyel@me.com>
2020-03-13 14:00:45 -0400

    project section more updates

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -6,18 +6,6 @@ import { NavLink } from "react-router-dom";
 const NavLinks = props => {
     return <ul className="nav-links">
         {
-        /* <li>
-            <NavLink to="/" exact> ALL USERS</NavLink>
-        </li>
-        <li>
-            <NavLink to="/ul/places">MY PLACES</NavLink>
-        </li>
-        <li>
-            <NavLink to="/places/new">ADD PLACE</NavLink>
-        </li>
-        <li>
-            <NavLink to="/auth">AUTHENTICATE</NavLink>
-        </li> */
         }
     </ul>
 }
